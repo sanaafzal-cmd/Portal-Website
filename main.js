03c0c24ef5b71a0a2419f907278e41f79faf5aa2
sanaafzal-cmd <105205204+sanaafzal-cmd@users.noreply.github.com>
2023-03-20 11:00:55 +0500

    20-march

--- main.js
+++ main.js
@@ -11,12 +11,14 @@ function toggleDivs() {
     toRent.classList.remove('is_active');
     toRent.classList.add('to-buy');
     div2.style.display = "none";
+    console.log("trig1");
   } else {
     div1.style.display = "none";
     div2.style.display = "block";
     toRent.classList.add('is_active');
     toBuy.classList.remove('is_active');
     toBuy.classList.add('to-buy');
+    console.log("trig2");
 
   }
 }
@@ -25,30 +27,10 @@ function toggleDivs() {
 document.getElementById("to-buy").addEventListener("click", toggleDivs);
 document.getElementById("to-rent").addEventListener("click", toggleDivs);
 
-// var box1 = document.querySelector('#box-1');
-// var box2 = document.querySelector('#box-2');
 
-// box2.style.display = "none";
-
-// if(box1) {
-//     box1.addEventListener('mouseenter', colorin);
-//     box1.addEventListener('mouseout', colorout);
-
-// }
-
-// function colorin() {
-//     box1.style.display = "none";
-//     box2.style.display="block";
-// }
-
-// function colorout() {
-//   box2.style.display = "none";
-//   box1.style.display="block";
-    
-// }
 
 var box2 = document.querySelector('#box-2');
-box2.style.display = "none";
+//box2.style.display = "none";
 $(document).ready(function()
 { $("#box-1").mouseover(function()
 {
@@ -61,6 +43,33 @@ $(document).ready(function()
   
    });});
 
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("loginBtn");
+console.log(btn)
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
    
 
-  
+
